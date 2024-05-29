45a303269407f09a6ca6df3c31acfbd6116d0428
SevroAuBarca <daniel.contreras1a@gmail.com>
2024-05-28 23:44:11 -0600

    Update Ingresar.jsx

--- src/Components/Ingresar.jsx
+++ src/Components/Ingresar.jsx
@@ -7,7 +7,7 @@ const Ingresar = () => {
     >
       <h3 className="font-bold text-xl my-3 lg:text-2xl">UNETE AL CLUB!</h3>
       <article className=" w-[160px] h-[200px] lg:w-[300px] lg:h-[500px] flex items-center">
-        <a href="">
+        <a href="https://forms.gle/zjUv7KNXCgBsQcRy6" target="_BLANK">
           <img src={qr} alt="" className="w-full" />
         </a>
       </article>
